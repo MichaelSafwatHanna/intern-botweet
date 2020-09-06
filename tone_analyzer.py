01bdd0a25014102bbc484c645ff727b9454204c5
MichaelSafwatHanna <michaelsafwat.hanna@gmail.com>
2020-09-06 12:59:06 +0200

    Add Document Tone Analyzer

---
+++ tone_analyzer.py
@@ -0,0 +1,28 @@
+import json
+import os
+from os.path import join
+
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson import ToneAnalyzerV3
+
+API_KEY = 'API KEY'
+SERVICE_URL = 'SERVICE_URL'
+
+authenticator = IAMAuthenticator(API_KEY)
+tone_analyzer = ToneAnalyzerV3(
+    version='2017-09-21',
+    authenticator=authenticator
+)
+tone_analyzer.set_service_url(SERVICE_URL)
+
+
+def analyze_document():
+    with open(join(os.getcwd(),
+                   'resources/tone-document.json')) as text:
+        tone = tone_analyzer.tone(
+            json.load(text)['text'],
+            content_type='text/html').get_result()
+
+    # print(json.dumps(tone, indent=2))
+    # print(json.dumps(tone['document_tone'], indent=2))
+    return tone['document_tone']
